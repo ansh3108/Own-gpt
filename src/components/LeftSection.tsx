5bd0a8659319c9acfb7cde1d1bb9b529d3ba696c
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-06 17:46:08 +0530

    Update LeftSection.tsx

--- src/components/LeftSection.tsx
+++ src/components/LeftSection.tsx
@@ -1,6 +1,31 @@
 import React from "react";
+import chatgptlogo from "@/assets/chatgptlogo.png";
+import nouserlogo from "@/assets/nouserlogo.png";
+import Image from "next/image";
 
 const LeftSection = () => {
+  const allChats = [
+    {
+      id: 1,
+      chatName: "This is a sample chat",
+    },
+    {
+      id: 2,
+      chatName: "This is a sample chat",
+    },
+    {
+      id: 3,
+      chatName: "This is a sample chat",
+    },
+    {
+      id: 4,
+      chatName: "This is a sample chat",
+    },
+    {
+      id: 6,
+      chatName: "This is a sample chat",
+    },
+  ];
   return <div>LeftSection</div>;
 };
 
